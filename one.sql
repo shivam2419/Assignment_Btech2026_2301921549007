2ef765562842a620475c60572980e6399cfd47bf
Shivam Sharma <120017051+shivam2419@users.noreply.github.com>
2025-01-26 18:25:44 +0530

    Create one.sql

---
+++ one.sql
@@ -0,0 +1,3 @@
+-- 1. Write a SQL query to fetch "FIRST_NAME" from the Student table in upper case and use ALIAS name as STUDENT_NAME.
+SELECT upper(FIRST_NAME) as STUDENT_NAME FROM student;
+SELECT lower(FIRST_NAME) as STUDENT_NAME FROM student;
